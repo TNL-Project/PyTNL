47d294ea590fb0e6edccc677f2dfadbe2146d9ba
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2025-06-03 13:41:10 +0200

    tests: fix Vector tests

--- tests/test_Vector.py
+++ tests/test_Vector.py
@@ -1,3 +1,5 @@
+import copy
+
 import pytest
 
 import tnl
@@ -9,6 +11,7 @@ def test_init(size):
     assert v.getSize() == size
 
 
+@pytest.mark.parametrize("size", [0, 1, 10, 100, 1000, 2**16])
 def test_square_brackets(size):
     v = tnl.Vector(size)
     for i in range(size):
@@ -17,6 +20,7 @@ def test_square_brackets(size):
         assert v[i] == i
 
 
+@pytest.mark.parametrize("size", [0, 1, 10, 100, 1000, 2**16])
 def test_addition(size):
     v1 = v2 = tnl.Vector(size)
     for i in range(size):
@@ -26,15 +30,33 @@ def test_addition(size):
     for i in range(size):
         assert v3[i] == v1[i] + v2[i]
 
-def test_negative_size():
+@pytest.mark.parametrize("size", [-1, -10, -100, -1000, -2**16])
+def test_negative_size(size):
     with pytest.raises(ValueError):
-        tnl.Vector(-1)
+        tnl.Vector(size)
 
+@pytest.mark.parametrize("size", [0, 1, 10, 100, 1000, 2**16])
 def test_copy(size):
     v1 = tnl.Vector(size)
     for i in range(size):
         v1[i] = i
-    v2 = v1.copy()
+    v2 = copy.copy(v1)
+    assert v1 is not v2
     assert v1 == v2
-    v2[0] = 99
-    assert v1 != v2
+    if size > 0:
+        v2[0] = 99
+        assert v1 != v2
+
+
+@pytest.mark.parametrize("size", [0, 1, 10, 100, 1000, 2**16])
+def test_assign(size):
+    v1 = tnl.Vector(size)
+    for i in range(size):
+        v1[i] = i
+    v2 = tnl.Vector()
+    v2.assign(v1)
+    assert v1 is not v2
+    assert v1 == v2
+    if size > 0:
+        v2[0] = 99
+        assert v1 != v2
