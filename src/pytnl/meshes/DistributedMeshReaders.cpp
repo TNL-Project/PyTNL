fa2ccb3884d98e4054270fa881f6481a72156801
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2025-06-23 16:20:30 +0200

    Add dummy overloads for PVTUReader::loadMesh bindings

--- src/pytnl/meshes/DistributedMeshReaders.cpp
+++ src/pytnl/meshes/DistributedMeshReaders.cpp
@@ -3,18 +3,42 @@
 
 #include <TNL/Meshes/Readers/PVTUReader.h>
 
+template< typename Mesh, typename Reader, typename... Args >
+void
+export_dummy_loadMesh( nb::class_< Reader, Args... >& reader )
+{
+   reader.def( "loadMesh",
+               []( const Reader& self, Mesh& mesh )
+               {
+                  throw std::logic_error( "cannot load non-distributed mesh using a distributed mesh reader" );
+               } );
+}
+
 void
 export_DistributedMeshReaders( nb::module_& m )
 {
    using XMLVTK = TNL::Meshes::Readers::XMLVTK;
    using PVTUReader = TNL::Meshes::Readers::PVTUReader;
 
-   nb::class_< PVTUReader, XMLVTK >( m, "PVTUReader" )
-      .def( nb::init< std::string >() )
-      // loadMesh is not virtual in PVTUReader
-      .def( "loadMesh", &PVTUReader::template loadMesh< DistributedMeshOfEdges > )
-      .def( "loadMesh", &PVTUReader::template loadMesh< DistributedMeshOfTriangles > )
-      .def( "loadMesh", &PVTUReader::template loadMesh< DistributedMeshOfQuadrangles > )
-      .def( "loadMesh", &PVTUReader::template loadMesh< DistributedMeshOfTetrahedrons > )
-      .def( "loadMesh", &PVTUReader::template loadMesh< DistributedMeshOfHexahedrons > );
+   auto reader =  //
+      nb::class_< PVTUReader, XMLVTK >( m, "PVTUReader" )
+         .def( nb::init< std::string >() )
+         // loadMesh is not virtual in PVTUReader
+         .def( "loadMesh", &PVTUReader::template loadMesh< DistributedMeshOfEdges > )
+         .def( "loadMesh", &PVTUReader::template loadMesh< DistributedMeshOfTriangles > )
+         .def( "loadMesh", &PVTUReader::template loadMesh< DistributedMeshOfQuadrangles > )
+         .def( "loadMesh", &PVTUReader::template loadMesh< DistributedMeshOfTetrahedrons > )
+         .def( "loadMesh", &PVTUReader::template loadMesh< DistributedMeshOfHexahedrons > );
+
+   // Add overloads for all types that loadMesh in the base class can handle to make mypy happy,
+   export_dummy_loadMesh< Grid1D >( reader );
+   export_dummy_loadMesh< Grid2D >( reader );
+   export_dummy_loadMesh< Grid3D >( reader );
+   export_dummy_loadMesh< MeshOfEdges >( reader );
+   export_dummy_loadMesh< MeshOfTriangles >( reader );
+   export_dummy_loadMesh< MeshOfQuadrangles >( reader );
+   export_dummy_loadMesh< MeshOfTetrahedrons >( reader );
+   export_dummy_loadMesh< MeshOfHexahedrons >( reader );
+   export_dummy_loadMesh< MeshOfPolygons >( reader );
+   export_dummy_loadMesh< MeshOfPolyhedrons >( reader );
 }
