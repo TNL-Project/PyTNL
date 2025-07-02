71b1abf5a83b2b641423765264277967ac16b99e
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2025-07-02 07:40:35 +0200

    Add type traits checks for binding vector operators

--- include/pytnl/containers/vector_operators.h
+++ include/pytnl/containers/vector_operators.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <TNL/TypeTraits.h>
 #include <pytnl/pytnl.h>
 
 template< typename VectorType, typename... Args >
@@ -25,205 +26,212 @@ def_vector_operators( nb::class_< VectorType, Args... >& vector )
             return self != other;
          },
          nb::sig( "def __ne__(self, arg: object, /) -> bool" ),
-         nb::is_operator() )
-      .def(
-         "__lt__",
-         []( const VectorType& self, const VectorType& other )
-         {
-            return self < other;
-         },
-         nb::is_operator() )
-      .def(
-         "__le__",
-         []( const VectorType& self, const VectorType& other )
-         {
-            return self <= other;
-         },
-         nb::is_operator() )
-      .def(
-         "__gt__",
-         []( const VectorType& self, const VectorType& other )
-         {
-            return self > other;
-         },
-         nb::is_operator() )
-      .def(
-         "__ge__",
-         []( const VectorType& self, const VectorType& other )
-         {
-            return self >= other;
-         },
-         nb::is_operator() )
+         nb::is_operator() );
 
-      // In-place arithmetic operators (Vector OP Vector)
-      .def(
-         "__iadd__",
-         []( VectorType& self, const VectorType& other ) -> VectorType&
-         {
-            self += other;
-            return self;
-         },
-         nb::is_operator() )
-      .def(
-         "__isub__",
-         []( VectorType& self, const VectorType& other ) -> VectorType&
-         {
-            self -= other;
-            return self;
-         },
-         nb::is_operator() )
-      .def(
-         "__imul__",
-         []( VectorType& self, const VectorType& other ) -> VectorType&
-         {
-            self *= other;
-            return self;
-         },
-         nb::is_operator() )
-      .def(
-         "__idiv__",
-         []( VectorType& self, const VectorType& other ) -> VectorType&
-         {
-            self /= other;
-            return self;
-         },
-         nb::is_operator() )
-
-      // In-place arithmetic operators (Vector OP Scalar)
-      .def(
-         "__iadd__",
-         []( VectorType& self, RealType scalar ) -> VectorType&
-         {
-            self += scalar;
-            return self;
-         },
-         nb::is_operator() )
-      .def(
-         "__isub__",
-         []( VectorType& self, RealType scalar ) -> VectorType&
-         {
-            self -= scalar;
-            return self;
-         },
-         nb::is_operator() )
-      .def(
-         "__imul__",
-         []( VectorType& self, RealType scalar ) -> VectorType&
-         {
-            self *= scalar;
-            return self;
-         },
-         nb::is_operator() )
-      .def(
-         "__idiv__",
-         []( VectorType& self, RealType scalar ) -> VectorType&
-         {
-            self /= scalar;
-            return self;
-         },
-         nb::is_operator() )
-
-      // Binary arithmetic operators (Vector OP Vector)
-      .def(
-         "__add__",
-         []( const VectorType& self, const VectorType& other )
-         {
-            return VectorType( self + other );
-         },
-         nb::is_operator() )
-      .def(
-         "__sub__",
-         []( const VectorType& self, const VectorType& other )
-         {
-            return VectorType( self - other );
-         },
-         nb::is_operator() )
-      .def(
-         "__mul__",
-         []( const VectorType& self, const VectorType& other )
-         {
-            return VectorType( self * other );
-         },
-         nb::is_operator() )
-      .def(
-         "__truediv__",
-         []( const VectorType& self, const VectorType& other )
-         {
-            return VectorType( self / other );
-         },
-         nb::is_operator() )
-
-      // Binary arithmetic operators (Vector OP Scalar)
-      .def(
-         "__add__",
-         []( const VectorType& self, RealType scalar )
-         {
-            return VectorType( self + scalar );
-         },
-         nb::is_operator() )
-      .def(
-         "__sub__",
-         []( const VectorType& self, RealType scalar )
-         {
-            return VectorType( self - scalar );
-         },
-         nb::is_operator() )
-      .def(
-         "__mul__",
-         []( const VectorType& self, RealType scalar )
-         {
-            return VectorType( self * scalar );
-         },
-         nb::is_operator() )
-      .def(
-         "__truediv__",
-         []( const VectorType& self, RealType scalar )
-         {
-            return VectorType( self / scalar );
-         },
-         nb::is_operator() )
-
-      // Reverse arithmetic operators (Scalar OP Vector)
-      .def(
-         "__radd__",
-         []( const VectorType& self, RealType scalar )
-         {
-            return VectorType( scalar + self );
-         },
-         nb::is_operator() )
-      .def(
-         "__rsub__",
-         []( const VectorType& self, RealType scalar )
-         {
-            return VectorType( scalar - self );
-         },
-         nb::is_operator() )
-      .def(
-         "__rmul__",
-         []( const VectorType& self, RealType scalar )
-         {
-            return VectorType( scalar * self );
-         },
-         nb::is_operator() )
-      .def(
-         "__rtruediv__",
-         []( const VectorType& self, RealType scalar )
-         {
-            return VectorType( scalar / self );
-         },
-         nb::is_operator() )
-
-      // Unary Operators
-      .def( "__pos__",
-            []( const VectorType& self )
+   if constexpr( TNL::IsScalarType< RealType >::value ) {
+      vector
+         .def(
+            "__lt__",
+            []( const VectorType& self, const VectorType& other )
             {
-               return VectorType( +self );
-            } )
-      .def( "__neg__",
-            []( const VectorType& self )
+               return self < other;
+            },
+            nb::is_operator() )
+         .def(
+            "__le__",
+            []( const VectorType& self, const VectorType& other )
             {
-               return VectorType( -self );
-            } );
+               return self <= other;
+            },
+            nb::is_operator() )
+         .def(
+            "__gt__",
+            []( const VectorType& self, const VectorType& other )
+            {
+               return self > other;
+            },
+            nb::is_operator() )
+         .def(
+            "__ge__",
+            []( const VectorType& self, const VectorType& other )
+            {
+               return self >= other;
+            },
+            nb::is_operator() );
+   }
+
+   if constexpr( TNL::IsScalarType< RealType >::value || TNL::is_complex_v< RealType > ) {
+      vector
+         // In-place arithmetic operators (Vector OP Vector)
+         .def(
+            "__iadd__",
+            []( VectorType& self, const VectorType& other ) -> VectorType&
+            {
+               self += other;
+               return self;
+            },
+            nb::is_operator() )
+         .def(
+            "__isub__",
+            []( VectorType& self, const VectorType& other ) -> VectorType&
+            {
+               self -= other;
+               return self;
+            },
+            nb::is_operator() )
+         .def(
+            "__imul__",
+            []( VectorType& self, const VectorType& other ) -> VectorType&
+            {
+               self *= other;
+               return self;
+            },
+            nb::is_operator() )
+         .def(
+            "__idiv__",
+            []( VectorType& self, const VectorType& other ) -> VectorType&
+            {
+               self /= other;
+               return self;
+            },
+            nb::is_operator() )
+
+         // In-place arithmetic operators (Vector OP Scalar)
+         .def(
+            "__iadd__",
+            []( VectorType& self, RealType scalar ) -> VectorType&
+            {
+               self += scalar;
+               return self;
+            },
+            nb::is_operator() )
+         .def(
+            "__isub__",
+            []( VectorType& self, RealType scalar ) -> VectorType&
+            {
+               self -= scalar;
+               return self;
+            },
+            nb::is_operator() )
+         .def(
+            "__imul__",
+            []( VectorType& self, RealType scalar ) -> VectorType&
+            {
+               self *= scalar;
+               return self;
+            },
+            nb::is_operator() )
+         .def(
+            "__idiv__",
+            []( VectorType& self, RealType scalar ) -> VectorType&
+            {
+               self /= scalar;
+               return self;
+            },
+            nb::is_operator() )
+
+         // Binary arithmetic operators (Vector OP Vector)
+         .def(
+            "__add__",
+            []( const VectorType& self, const VectorType& other )
+            {
+               return VectorType( self + other );
+            },
+            nb::is_operator() )
+         .def(
+            "__sub__",
+            []( const VectorType& self, const VectorType& other )
+            {
+               return VectorType( self - other );
+            },
+            nb::is_operator() )
+         .def(
+            "__mul__",
+            []( const VectorType& self, const VectorType& other )
+            {
+               return VectorType( self * other );
+            },
+            nb::is_operator() )
+         .def(
+            "__truediv__",
+            []( const VectorType& self, const VectorType& other )
+            {
+               return VectorType( self / other );
+            },
+            nb::is_operator() )
+
+         // Binary arithmetic operators (Vector OP Scalar)
+         .def(
+            "__add__",
+            []( const VectorType& self, RealType scalar )
+            {
+               return VectorType( self + scalar );
+            },
+            nb::is_operator() )
+         .def(
+            "__sub__",
+            []( const VectorType& self, RealType scalar )
+            {
+               return VectorType( self - scalar );
+            },
+            nb::is_operator() )
+         .def(
+            "__mul__",
+            []( const VectorType& self, RealType scalar )
+            {
+               return VectorType( self * scalar );
+            },
+            nb::is_operator() )
+         .def(
+            "__truediv__",
+            []( const VectorType& self, RealType scalar )
+            {
+               return VectorType( self / scalar );
+            },
+            nb::is_operator() )
+
+         // Reverse arithmetic operators (Scalar OP Vector)
+         .def(
+            "__radd__",
+            []( const VectorType& self, RealType scalar )
+            {
+               return VectorType( scalar + self );
+            },
+            nb::is_operator() )
+         .def(
+            "__rsub__",
+            []( const VectorType& self, RealType scalar )
+            {
+               return VectorType( scalar - self );
+            },
+            nb::is_operator() )
+         .def(
+            "__rmul__",
+            []( const VectorType& self, RealType scalar )
+            {
+               return VectorType( scalar * self );
+            },
+            nb::is_operator() )
+         .def(
+            "__rtruediv__",
+            []( const VectorType& self, RealType scalar )
+            {
+               return VectorType( scalar / self );
+            },
+            nb::is_operator() )
+
+         // Unary Operators
+         .def( "__pos__",
+               []( const VectorType& self )
+               {
+                  return VectorType( +self );
+               } )
+         .def( "__neg__",
+               []( const VectorType& self )
+               {
+                  return VectorType( -self );
+               } );
+   }
 
    // Additional operators defined only for integral value types
    if constexpr( std::is_integral_v< RealType > ) {
