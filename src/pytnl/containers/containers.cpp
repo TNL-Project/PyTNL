5b08bb90fb15f2a6db63da897fa885e16245fb7b
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2025-12-06 14:02:27 +0100

    Add initial bindings for NDArrayView

--- src/pytnl/containers/containers.cpp
+++ src/pytnl/containers/containers.cpp
@@ -38,6 +38,9 @@ using _ndarray = NDArray< T,
                           //ConstStaticSizesHolder< IndexType, dim, 0 >  // ConstStaticSizesHolder does not have Python bindings
                           >;
 
+template< int dim, typename T >
+using _ndarray_view = typename _ndarray< dim, T >::ViewType;
+
 // Python module definition
 NB_MODULE( _containers, m )
 {
@@ -90,4 +93,24 @@ NB_MODULE( _containers, m )
    export_NDArray< _ndarray< 1, ComplexType > >( m, "NDArray_1_complex" );
    export_NDArray< _ndarray< 2, ComplexType > >( m, "NDArray_2_complex" );
    export_NDArray< _ndarray< 3, ComplexType > >( m, "NDArray_3_complex" );
+
+   export_NDArray< _ndarray_view< 1, IndexType > >( m, "NDArrayView_1_int" );
+   export_NDArray< _ndarray_view< 2, IndexType > >( m, "NDArrayView_2_int" );
+   export_NDArray< _ndarray_view< 3, IndexType > >( m, "NDArrayView_3_int" );
+   export_NDArray< _ndarray_view< 1, RealType > >( m, "NDArrayView_1_float" );
+   export_NDArray< _ndarray_view< 2, RealType > >( m, "NDArrayView_2_float" );
+   export_NDArray< _ndarray_view< 3, RealType > >( m, "NDArrayView_3_float" );
+   export_NDArray< _ndarray_view< 1, ComplexType > >( m, "NDArrayView_1_complex" );
+   export_NDArray< _ndarray_view< 2, ComplexType > >( m, "NDArrayView_2_complex" );
+   export_NDArray< _ndarray_view< 3, ComplexType > >( m, "NDArrayView_3_complex" );
+
+   export_NDArray< _ndarray_view< 1, IndexType const > >( m, "NDArrayView_1_int_const" );
+   export_NDArray< _ndarray_view< 2, IndexType const > >( m, "NDArrayView_2_int_const" );
+   export_NDArray< _ndarray_view< 3, IndexType const > >( m, "NDArrayView_3_int_const" );
+   export_NDArray< _ndarray_view< 1, RealType const > >( m, "NDArrayView_1_float_const" );
+   export_NDArray< _ndarray_view< 2, RealType const > >( m, "NDArrayView_2_float_const" );
+   export_NDArray< _ndarray_view< 3, RealType const > >( m, "NDArrayView_3_float_const" );
+   export_NDArray< _ndarray_view< 1, ComplexType const > >( m, "NDArrayView_1_complex_const" );
+   export_NDArray< _ndarray_view< 2, ComplexType const > >( m, "NDArrayView_2_complex_const" );
+   export_NDArray< _ndarray_view< 3, ComplexType const > >( m, "NDArrayView_3_complex_const" );
 }
--- src/pytnl/containers/containers.cu
+++ src/pytnl/containers/containers.cu
@@ -31,6 +31,9 @@ using _ndarray = NDArray< T,
                           //ConstStaticSizesHolder< IndexType, dim, 0 >  // ConstStaticSizesHolder does not have Python bindings
                           >;
 
+template< int dim, typename T >
+using _ndarray_view = typename _ndarray< dim, T >::ViewType;
+
 // Python module definition
 NB_MODULE( _containers_cuda, m )
 {
@@ -75,4 +78,24 @@ NB_MODULE( _containers_cuda, m )
    export_NDArray< _ndarray< 1, ComplexType > >( m, "NDArray_1_complex" );
    export_NDArray< _ndarray< 2, ComplexType > >( m, "NDArray_2_complex" );
    export_NDArray< _ndarray< 3, ComplexType > >( m, "NDArray_3_complex" );
+
+   export_NDArray< _ndarray_view< 1, IndexType > >( m, "NDArrayView_1_int" );
+   export_NDArray< _ndarray_view< 2, IndexType > >( m, "NDArrayView_2_int" );
+   export_NDArray< _ndarray_view< 3, IndexType > >( m, "NDArrayView_3_int" );
+   export_NDArray< _ndarray_view< 1, RealType > >( m, "NDArrayView_1_float" );
+   export_NDArray< _ndarray_view< 2, RealType > >( m, "NDArrayView_2_float" );
+   export_NDArray< _ndarray_view< 3, RealType > >( m, "NDArrayView_3_float" );
+   export_NDArray< _ndarray_view< 1, ComplexType > >( m, "NDArrayView_1_complex" );
+   export_NDArray< _ndarray_view< 2, ComplexType > >( m, "NDArrayView_2_complex" );
+   export_NDArray< _ndarray_view< 3, ComplexType > >( m, "NDArrayView_3_complex" );
+
+   export_NDArray< _ndarray_view< 1, IndexType const > >( m, "NDArrayView_1_int_const" );
+   export_NDArray< _ndarray_view< 2, IndexType const > >( m, "NDArrayView_2_int_const" );
+   export_NDArray< _ndarray_view< 3, IndexType const > >( m, "NDArrayView_3_int_const" );
+   export_NDArray< _ndarray_view< 1, RealType const > >( m, "NDArrayView_1_float_const" );
+   export_NDArray< _ndarray_view< 2, RealType const > >( m, "NDArrayView_2_float_const" );
+   export_NDArray< _ndarray_view< 3, RealType const > >( m, "NDArrayView_3_float_const" );
+   export_NDArray< _ndarray_view< 1, ComplexType const > >( m, "NDArrayView_1_complex_const" );
+   export_NDArray< _ndarray_view< 2, ComplexType const > >( m, "NDArrayView_2_complex_const" );
+   export_NDArray< _ndarray_view< 3, ComplexType const > >( m, "NDArrayView_3_complex_const" );
 }
