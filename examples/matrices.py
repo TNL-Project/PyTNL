2c2b1d8bbe15a0d9d1bc72e0835aa12445dc3d91
fabijannn <fabianuhrin1@gmail.com>
2025-12-17 19:15:08 +0100

    filling the matrix and cleanup

--- examples/matrices.py
+++ examples/matrices.py
@@ -1,40 +1,56 @@
 from pytnl.matrices import CSR, Ellpack, SparseMatrixRowView
 from pytnl._containers import Vector_int
 import numpy as np
-# import scipy as sp
-import sys
 import time
-
+import random
 
 # TODO
 # 1. addition
 # 2. multiplication
 # 3. inverse
 # 4. norm
-# 5.
+# 5. load
+# 6. save
+# 7. reset
 
 # region pytnl
 
-a_matrix = CSR()
-a_vector = Vector_int(10, 50)
-a_matrix.setDimensions(10, 10)
-a_matrix.setRowCapacities(a_vector)
+# create a compressed sparse row matrix
+tnl_matrix = CSR()
+tnl_vector = Vector_int(10, 10)
+tnl_matrix.setDimensions(10, 10)
+tnl_matrix.setRowCapacities(tnl_vector)
+# tnl_matrix.addElement(3, 5, 5.2, 1)
 
-a_matrix.addElement(3, 5, 5.2)
-# a_matrix.print()
+# fill the matrix 
+val = random.uniform(0, 2)
 
+for i in range(10):
+    for j in range(10):
+        if random.random() < 0.1:
+            val = random.random()
+            tnl_matrix.addElement(i, j, val, 1)
 
-columns = a_matrix.getColumns()
-rows = a_matrix.getRows()
+# rows, cols = tnl_matrix.getRows(), tnl_matrix.getColumns()
+# dense = np.zeros((rows, cols))
 
-print("columns:", columns, "rows:", rows, "matrix:", a_matrix)
+# for r in range(rows):
+#     row_data = tnl_matrix.getRow(r)
+#     for col, val in row_data:
+#         dense[r, col] = val
+
+columns = tnl_matrix.getColumns()
+rows = tnl_matrix.getRows()
+
+print("columns:", columns, "rows:", rows, "matrix:", tnl_matrix)
 
 # endregion
 
+
 # region numpy
 
-# b_matrix = np.array()
-# b_matrix([1, 0, 0], [0, 1, 0], [0, 0, 1])
+b_matrix = np.array()
+b_matrix([1, 0, 0], [0, 1, 0], [0, 0, 1])
 
 
 # endregion
