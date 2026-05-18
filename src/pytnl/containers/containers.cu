1dcb99c8c5a7a3d273a6d3ddd8108b410cd57690
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2026-05-18 15:09:52 +0200

    Call TNL::MPI::selectGPU from the lowest-level GPU-accelerated module

--- src/pytnl/containers/containers.cu
+++ src/pytnl/containers/containers.cu
@@ -1,6 +1,8 @@
 #include <pytnl/exceptions.h>
 #include <pytnl/pytnl.h>
 
+#include <TNL/MPI/Utils.h>
+
 void
 export_ArrayVector( nb::module_& m );
 void
@@ -13,6 +15,13 @@ NB_MODULE( _containers_cuda, m )
 
    // import depending modules
    nb::module_::import_( "pytnl._containers" );
+   nb::module_::import_( "mpi4py.MPI" );
+
+   // Importing mpi4py.MPI does MPI_Init, but it does not handle GPU selection.
+   // Calling selectGPU() from the lowest-level GPU-accelerated module ensures
+   // the same behavior as the TNL::MPI::Initialize wrapper function.
+   if( TNL::MPI::isInitialized() )
+      TNL::MPI::selectGPU();
 
    export_ArrayVector( m );
    export_NDArray( m );
